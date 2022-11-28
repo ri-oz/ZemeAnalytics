89651cdbf6b62a76c4c5c9b0cc4d5e3cc09a7659
ri-oz <61837174+ri-oz@users.noreply.github.com>
2022-11-28 19:37:42 +0200

    Update ZemeSSAnalytics.py

--- ZemeSSAnalytics.py
+++ ZemeSSAnalytics.py
@@ -3,15 +3,14 @@
 #%%
 
 import pandas as pd
-
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import pygsheets
 import streamlit as st
 import numpy as np
 from datetime import datetime
+import matplotlib.pyplot as plt
 
-#%%
  
 gc = pygsheets.authorize(service_file='/Users/rioz/Documents/GitHub/ZemeAnalytics/research-python-gs.json')
 
@@ -23,10 +22,51 @@ sh = gc.open('Py_land_data')
 
 wks = sh[0]
 
+# create dataframe
+
 df_Zeme = pd.DataFrame(wks.get_all_records())
 
+#df_Zeme_analytics_Cena_bins = df_Zeme['Cena EUR'].value_counts(bins=20)
+
+df_Zeme_analytics_Pilseta_skaits = df_Zeme['Pilseta'].value_counts(ascending=True)
+
+df_Zeme_analytics_Tips_skaits = df_Zeme['Zemes Tips'].value_counts(ascending=True)
 
 
-# %%
+# Title
 
+st.title('Zemes Cenu pārskats Latvijā')
+
+
+# Description
+
+st.text('Datu analīzes projekts par zemes pārdošanu un cenām Latvijā.')
+
+
+# Create a section for the dataframe statistics
+
+st.header('Datu statistiskā anaīze')
+st.write(df_Zeme.describe())
+
+# Create a section for the dataframe
+st.header('Sludinājumu dati')
+st.dataframe(df_Zeme)
+
+
+# City overview section
+
+st.header('Pilsētu pārskats')
+
+st.bar_chart(df_Zeme_analytics_Pilseta_skaits)
+
+st.dataframe(df_Zeme_analytics_Pilseta_skaits)
+
+
+# Type of land overview question
+
+st.header('Zemes Tipi')
+
+st.bar_chart(df_Zeme_analytics_Tips_skaits)
+
+st.dataframe(df_Zeme_analytics_Tips_skaits)
 
