997de86fdec128ebb2887ab965cc9354957446f4
ri-oz <61837174+ri-oz@users.noreply.github.com>
2022-11-29 23:07:30 +0200

    Update ZemeSSAnalytics.py

--- ZemeSSAnalytics.py
+++ ZemeSSAnalytics.py
@@ -169,45 +169,16 @@ c = alt.Chart(chart_data).mark_circle().encode(
 
 st.altair_chart(c, use_container_width=True)
 
-Cenas_tips = st.radio(
-    "Datu kategorija",
-    ('Pilsetu Dati', 'Pielietojuma dati'))
-if Cenas_tips == 'Pilsetu Dati':
-    st.dataframe(df_pilseta)
-    
-else:
-    st.dataframe(df_tips)
-
 
 # %%
+tab1, tab2 = st.tabs(["Pilsetu Dati", "Pielietojuma Dati"])
 
-source = df_pilseta
+with tab1:
+   st.header("Pilsetu Dati")
+   st.dataframe(df_pilseta)
 
-# Configure the options common to all layers
-brush = alt.selection(type='interval')
-base = alt.Chart(source).add_selection(brush)
+with tab2:
+   st.header("Pielietojuma Dati")
+   st.dataframe(df_tips)
 
-# Configure the points
-points = base.mark_point().encode(
-    x=alt.X('Videja Cena EUR', title=''),
-    y=alt.Y('Videja Platiba Daudzums', title=''),
-    color=alt.condition(brush, 'Pilseta', alt.value('grey'))
-)
 
-# Configure the ticks
-tick_axis = alt.Axis(labels=False, domain=False, ticks=False)
-
-x_ticks = base.mark_tick().encode(
-    alt.X('Videja Cena EUR', axis=tick_axis),
-    alt.Y('Pilseta', title='', axis=tick_axis),
-    color=alt.condition(brush, 'Pilseta', alt.value('lightgrey'))
-)
-
-y_ticks = base.mark_tick().encode(
-    alt.X('Pilseta', title='', axis=tick_axis),
-    alt.Y('Videja Platiba Daudzums', axis=tick_axis),
-    color=alt.condition(brush, 'Pilseta', alt.value('lightgrey'))
-)
-
-# Build the chart
-y_ticks | (points & x_ticks)
